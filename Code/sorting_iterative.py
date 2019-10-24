7814b2575cfa95217da979cb373eb78c17940659
LucHighwalker <luc.highwalker@gmail.com>
2019-10-24 12:03:00 -0700

    completed is_sorted

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -3,9 +3,16 @@
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n) n = length of items. Since it has to run through the list
+    TODO: Memory usage: O(1) it doesn't create any new variables except for keeping track of the previous item"""
     # TODO: Check that all adjacent items are in order, return early if so
+    prev = None
+    for item in items:
+      if prev != None and item < prev:
+        return False
+      prev = item
+    return True
+
 
 
 def bubble_sort(items):
