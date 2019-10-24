f8d90182be3debb0c33bea39ce032732ea821f75
LucHighwalker <luc.highwalker@gmail.com>
2019-10-24 14:23:53 -0700

    completed selection and insertion sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -20,27 +20,27 @@ def bubble_sort(items):
     TODO: Running time: O(n^2) since if all items are out of order, it will have to perform n operations on n items
     TODO: Memory usage: O(1) since it only manipulates the input variable without creating a new list"""
     sortd = False
-    counter = 0
-    counter_mod = 1
+    index = 0
+    index_mod = 1
     swapped = False
     while not sortd:
-      if counter < len(items) - counter_mod:
-        item = items[counter]
-        adj = items[counter + 1]
+      if index < len(items) - index_mod:
+        item = items[index]
+        adj = items[index + 1]
 
         if item > adj:
           swapped = True
-          items[counter] = adj
-          items[counter + 1] = item
-          if counter + 1 == len(items) - counter_mod:
-            counter_mod += 1
+          items[index] = adj
+          items[index + 1] = item
+          if index + 1 == len(items) - index_mod:
+            index_mod += 1
         
-        counter += 1
+        index += 1
       else:
         if not swapped:
           sortd = True
         
-        counter = 0
+        index = 0
         swapped = False
 
 
@@ -50,9 +50,24 @@ def selection_sort(items):
     unsorted item, and repeating until all items are in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    sortd = len(items) < 1
+    already_sorted = 0
+    while not sortd:
+      min_val_index = None
+      for index in range(already_sorted, len(items)):
+        if min_val_index == None or items[index] < items[min_val_index]:
+          min_val_index = index
+      first_unsorted = items[already_sorted]
+      item_to_swap = items[min_val_index]
+
+      items[min_val_index] = first_unsorted
+      items[already_sorted] = item_to_swap
+
+      already_sorted += 1
+
+      if already_sorted == len(items):
+        sortd = True
+
 
 
 def insertion_sort(items):
@@ -64,9 +79,22 @@ def insertion_sort(items):
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
 
+    already_sorted = 0
+    for i in range(len(items)):
+      item = items[i]
+      for j in range(already_sorted - 1, -1, -1):
+        if items[j] > item:
+          items[j+1] = items[j]
+          items[j] = item
+        else:
+          break
+      already_sorted += 1
+
 
 # if __name__ == '__main__':
 #   l = [10, 420, 20, 10, 600, 1, 1, 430, 520, 102, 320, 21, 415, 4]
 #   print(l)
-#   bubble_sort(l)
+#   # bubble_sort(l)
+#   # selection_sort(l)
+#   insertion_sort(l)
 #   print(l)
